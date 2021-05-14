b5dc453c156e4009c641a49bacc8730fe9bd58a9
changebaobao <76760881+changebaobao@users.noreply.github.com>
2021-05-14 20:34:38 +0800

    Add files via upload

---
+++ java基础/TCP通信程序练习/上传文件服务器给出反馈/ServerDemo.java
@@ -0,0 +1,36 @@
+package ppp;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*服务器：接收到的数据写入文本文件，给出反馈*/
+public class ServerDemo {
+    public static void main(String[] args) throws IOException {
+        //创建服务器Socket对象
+        ServerSocket ss=new ServerSocket(9000);
+        //监听客户端连接，返回一个对应的Socket对象
+        Socket s=ss.accept();
+        //接收数据
+        BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+        //把数据写入文本文件
+        BufferedWriter bw=new BufferedWriter(new FileWriter("src\\ppp\\123.txt"));
+        String line;
+        while ((line=br.readLine())!=null){
+            if ("886".equals(line)){
+                break;
+            }
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+        //给出反馈
+        BufferedWriter bwServer=new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+        bwServer.write("文件上传成功");
+        bwServer.newLine();
+        bwServer.flush();
+        //释放资源
+        bw.close();
+        ss.close();
+    }
+}
